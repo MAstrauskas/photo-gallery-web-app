153d28a625a4b527e9494e93cafaac54e9afd6a1
Mantas Astrauskas <mastrauskas4@gmail.com>
2020-04-01 15:10:39 +0100

    Created 1st web server

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprint(w, "<h1>Welcome to the site!</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", handlerFunc)
+	_ = http.ListenAndServe(":3000", nil)
+}
