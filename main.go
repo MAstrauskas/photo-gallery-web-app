74ff79b006a23675e9de7e9b3b698ee8e6e8c891
Mantas Astrauskas <mastrauskas4@gmail.com>
2020-04-02 16:05:01 +0100

    Add routes

--- main.go
+++ main.go
@@ -7,10 +7,17 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	_, _ = fmt.Fprint(w, "<h1>Welcome to the site!</h1>")
+
+	if r.URL.Path == "/" {
+		_, _ = fmt.Fprint(w, "<h1>Welcome to the site!</h1>")
+	} else if r.URL.Path == "/contact" {
+		_, _ = fmt.Fprint(w, "To get in touch, please send an email to "+
+			"<a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
+	}
 }
 
 func main() {
 	http.HandleFunc("/", handlerFunc)
 	_ = http.ListenAndServe(":3000", nil)
 }
+
