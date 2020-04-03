9ef119136f507c2ef91845c40d69ac15e3672c3d
Mantas Astrauskas <mastrauskas4@gmail.com>
2020-04-03 16:36:47 +0100

    Refactor to use gorilla mux

--- main.go
+++ main.go
@@ -2,25 +2,28 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 
-	if r.URL.Path == "/" {
-		_, _ = fmt.Fprint(w, "<h1>Welcome to the site!</h1>")
-	} else if r.URL.Path == "/contact" {
-		_, _ = fmt.Fprint(w, "To get in touch, please send an email to "+
-			"<a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		_, _ = fmt.Fprint(w, "<h1>Sorry, the page you requested does not exist.</h1><p>Please email us if you keep being sent to an invalid page.</p>")
-	}
+	_, _ = fmt.Fprint(w, "<h1>Welcome to the site!</h1>")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	_, _ = fmt.Fprint(w, "To get in touch, please send an email to "+
+		"<a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	_ = http.ListenAndServe(":3000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+
+	_ = http.ListenAndServe(":3000", r)
 }
 
