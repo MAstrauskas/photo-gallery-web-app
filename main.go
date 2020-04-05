3cf3e49594108c2ba27da698612afa43f60e94ad
Mantas Astrauskas <mastrauskas4@gmail.com>
2020-04-05 16:08:31 +0100

    Add FAQ and custom error handler

--- main.go
+++ main.go
@@ -19,11 +19,28 @@ func contact(w http.ResponseWriter, r *http.Request) {
 		"<a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
 }
 
-func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", home)
-	r.HandleFunc("/contact", contact)
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
 
-	_ = http.ListenAndServe(":3000", r)
+	_, _ = fmt.Fprint(w, "<h1>Frequently Asked Questions</h1>" +
+		"<h2>What is this website about?</h2>" +
+		"<p>It's about learning to build real world applications using Go (Golang)</p>")
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	_, _ = fmt.Fprint(w, "<h1>404 page not found<h1>" +
+		"<h2>The page you were trying to access does not exist.</h2>")
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", home)
+	router.HandleFunc("/contact", contact)
+	router.HandleFunc("/faq", faq)
+	router.NotFoundHandler = http.HandlerFunc(notFound)
+
+	_ = http.ListenAndServe(":3000", router)
 }
 
